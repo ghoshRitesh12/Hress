a214a1d2c10ce8dbf5137ebcb47017663dc4457c
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-06-25 19:46:40 +0530

    feat: added login page validation schema

---
+++ utils/loginSchema.js
@@ -0,0 +1,12 @@
+import { toTypedSchema } from '@vee-validate/yup';
+import { object, string } from 'yup';
+
+
+export const loginSchema = toTypedSchema(object({
+  email: string().trim().required('Email id is required').email('invalid email address'),
+  password: string().trim().required().matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*()])(?=.{8,16}$)/,
+    'Use 8 to 16 characters with a mix of letters(lower & upper case), numbers & symbols among !@#$%^&*()'
+  )
+}))
+
