d04c29cef23bc3d9d9cf1c74ff58a5524c3fe016
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-06-25 19:43:48 +0530

    refactor: user model

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -16,6 +16,12 @@ const userSchema = new mongoose.Schema({
       // trim: true,
       // unique: true
     },
+    pfp: {
+      type: String,
+      default: () => 
+        'https://api.dicebear.com/6.x/bottts/png'+`?seed=${Date.now()}&eyes=shade01`+
+        '&mouth=smile01&texture=circuits'+'&face=round02&sides=square'
+    },
     referralId: {
       type: String,
       default: () => randomBytes(
@@ -31,20 +37,22 @@ const userSchema = new mongoose.Schema({
       type: Boolean,
       default: false,
     },
-    panCardNo: {
+    courseType: {
       type: String,
-      default: null
+      required: true
     },
-    bankInfo: {
-      accountNo: {
-        type: Number,
-        default: null
-      },
-      ifsc: {
-        type: String,
-        default: null
-      }
+
+    personalInfo: {
+      pancardNo: { type: String, default: null },
+      phoneNumber: { type: Number, default: null },
+      country: { type: String, default: null },
+      postalCode: { type: String, default: null },
+      cityState: { type: String, default: null },
+      streetAddress: { type: String, default: null },
+      bankAccountNo: { type: Number, default: null },
+      ifsc: { type: String, default: null }
     },
+
     rank: {
       type: Number,
       default: 1,
@@ -76,6 +84,8 @@ const userSchema = new mongoose.Schema({
     collection: collectionName
   }
 )
+
+// userSchema.pre('')
 
 
 export default mongoose.model(collectionName, userSchema);
