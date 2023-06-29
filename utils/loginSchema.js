31c882594ed0d9f7c0ba7d6d6a48f2a49635a41e
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-06-30 02:21:59 +0530

    refactor: login schema

--- utils/loginSchema.js
+++ utils/loginSchema.js
@@ -2,11 +2,19 @@ import { toTypedSchema } from '@vee-validate/yup';
 import { object, string } from 'yup';
 
 
-export const loginSchema = toTypedSchema(object({
+export const clientLoginSchema = toTypedSchema(object({
   email: string().trim().required('Email id is required').email('invalid email address'),
   password: string().trim().required().matches(
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*()])(?=.{8,16}$)/,
     'Use 8 to 16 characters with a mix of letters(lower & upper case), numbers & symbols among !@#$%^&*()'
   )
 }))
+
+export const serverLoginSchema = object({
+  email: string().trim().required('Email id is required').email('invalid email address'),
+  password: string().trim().required().matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*()])(?=.{8,16}$)/,
+    'Use 8 to 16 characters with a mix of letters(lower & upper case), numbers & symbols among !@#$%^&*()'
+  )
+})
 
