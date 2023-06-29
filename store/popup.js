42bc1413ca4791ac8d86925a382e7ef587bc72c9
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-06-29 14:03:38 +0530

    feat: added popup store

--- store/popup.js
+++ store/popup.js
@@ -7,7 +7,7 @@ export const isPopupOpen = ref(false);
 export function setPopupMessage(message) {
   popupMessage.value = message;
   isPopupOpen.value = true;
-}
+} 
 
 
 export function closePopup() {
