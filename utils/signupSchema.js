bb8792bb9d96ccdd502886b49f82c32ad4588fec
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-06-29 18:15:10 +0530

    feat: added server signup schema

--- utils/signupSchema.js
+++ utils/signupSchema.js
@@ -1,13 +1,12 @@
 import { toTypedSchema } from '@vee-validate/yup';
 import { object, string, ref as yupRef } from 'yup';
 
-export const signupSchema = [
+export const clientSignupSchema = [
   toTypedSchema(object().shape({
       fullname: string().trim().required('Full name is required').min(3, 'invalid name'),
       email: string().trim().required('Email address is required').email('invalid email id'),
       course: string().required('Select a course').equals(['basic', 'advance']),
     }, 
-    [['refererId', 'refererId']]
   )),
 
   toTypedSchema(object().shape({
@@ -18,7 +17,10 @@ export const signupSchema = [
           return string().notRequired()
         }
       }),
-      sponsorerId: string().trim().required().length(10).notOneOf([ref('refererId'), null], 'Sponsorer & referer id must be different'),
+      sponsorerId: string().trim().required().length(
+        10,
+        'Sponsorer id must be exactly 10 characters'
+      ).notOneOf([yupRef('refererId'), null], 'Sponsorer & referer id must be different'),
     }, 
     [['refererId', 'refererId']]
   )),
@@ -30,13 +32,40 @@ export const signupSchema = [
       ),
       confirmPassword: string().trim().required('Re enter password').oneOf([yupRef('password')], 'Passwords must match')
     }, 
-    [['refererId', 'refererId']]
   )),
 
   toTypedSchema(object().shape({
       otp: string().required('OTP is required').length(6)
     }, 
-    [['refererId', 'refererId']]
   )),
 ]
 
+
+// export const serverSignupSchema = '';
+export const serverSignupSchema = object().shape({
+    fullname: string().trim().required('Full name is required').min(3, 'invalid name'),
+    email: string().trim().required('Email address is required').email('invalid email id'),
+    course: string().required('Select a course').equals(['basic', 'advance']),
+    refererId: string().trim().when(['refererId'], (val, schema) => {
+      if(val[0]?.trim()?.length > 0) {
+        return string().length(
+          10, 
+          'Referer id must be exactly 10 characters'
+        )
+      } else {
+        return string().notRequired()
+      }
+    }),
+    sponsorerId: string().trim().required().length(
+      10,
+      'Sponsorer id must be exactly 10 characters'
+    ).notOneOf([yupRef('refererId'), null], 'Sponsorer & referer id must be different'),
+    password: string().trim().required('Password required').matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*()])(?=.{8,16}$)/,
+      'Use 8 to 16 characters with a mix of letters(lower & upper case), numbers & symbols among !@#$%^&*()'
+    ),
+    confirmPassword: string().trim().required('Re enter password').oneOf([yupRef('password')], 'Passwords must match'),
+    otp: string().required('OTP is required').length(6)
+  },
+  [['refererId', 'refererId']]
+)
