ba91fbc1d63dbde95d58df2d040b45253069c39d
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-06-29 18:18:58 +0530

    feat: added server data validation

--- server/api/auth/signup.post.js
+++ server/api/auth/signup.post.js
@@ -1,4 +1,5 @@
 import { startSession } from "mongoose";
+import { serverSignupSchema } from "~/utils/signupSchema";
 import User from "../../models/User.model";
 import Otp from "../../models/Otp.model";
 import { getIncentive, isRankValid } from "../../helpers/utils.js";
@@ -10,35 +11,46 @@ export default eventHandler(async (event) => {
   
   try {
     const body = await readBody(event);
-    console.log(body);
-    // verify body schema
-    // use passport
- 
-    
+    await serverSignupSchema.validate(body);    
+
+    const tempUser = await Otp.findOne({ email: body.email });
+    if(!tempUser) {
+      return sendError(event, createError({
+        statusCode: 401, 
+        statusMessage: 'Otp has expired, try 10 mins later',
+      }))
+    }
+    if(tempUser.otp !== parseInt(body.otp)) {
+      return sendError(event, createError({
+        statusCode: 403, 
+        statusMessage: 'Invalid OTP, try 10 mins later',
+      }))
+    }
+    await Otp.deleteOne({ email: body.email });
 
     
-    const foundUser = await User.findOne({ email: body.email }).session(signupSession);
-    if(foundUser) {
-      throw sendError(event, createError({
+
+    if(await User.exists({ email: body.email }).session(signupSession)) {
+      return sendError(event, createError({
         statusCode: 409, 
-        message: 'User already exists',
+        statusMessage: 'User already exists',
       })) 
     }
 
+    const spillOverIncentive = 20;
     const sponsorer = await User.findOne({ referralId: body.sponsorerId }).session(signupSession);
     if(!sponsorer) {
-      throw createError({
+      return createError({
         statusCode: 404,
-        message: 'Sponsorer not found'
+        statusMessage: 'Sponsorer not found'
       })
     }
-    const spillOverIncentive = 20;
-    
     
     const newUserInfo = [{
       name: body.fullname,
       email: body.email,
       courseType: body.course,
+      verified: true,
       ancestors: [...sponsorer.ancestors, `${sponsorer._id}`].sort()
     }]
     const newUser = (await User.create(newUserInfo, { session: signupSession }))[0];
@@ -46,7 +58,6 @@ export default eventHandler(async (event) => {
     // for each ancestor
     const ancestors = await User.find({ _id: { $in: newUser.ancestors } }).session(signupSession);
     const sponsorAncestorReferralIds = ancestors.slice(0, -1).map(a => a.referralId);
-    console.log(sponsorAncestorReferralIds);
 
     const updateOperations = ancestors.map((ancestor, index) => {
       const indexedLevel = ancestors.length - index;
@@ -110,17 +121,17 @@ export default eventHandler(async (event) => {
 
     setResponseStatus(event, 201);
     return {
-      message: 'Account created successfully'
+      message: 'Account created successfully 🎉'
     };
     
   } catch (err) {
-    await signupSession.abortTransaction();
     console.log(err);
+    await signupSession.abortTransaction();
 
-    setResponseStatus(event, 500);
-    return { 
-      errorMessage: 'Account creation unsuccessful, try again' 
-    };
+    return sendError(event, createError({
+      statusCode: 500,
+      statusMessage: 'Account creation unsuccessful, try again'
+    }))
 
   } finally {
     await signupSession.endSession();
