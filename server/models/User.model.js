3ef3d3b72ef8dd3b09f5b51d50d1bd9a88b76a2e
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-07-10 12:07:35 +0530

    refactor: user model

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -1,64 +1,52 @@
-import { randomBytes } from 'crypto';
 import mongoose from "mongoose";
+import { randomBytes } from "crypto";
 
 const collectionName = 'Users';
 
 const userSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      trim: true
-    },
-    email: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    password: {
-      type: String,
-      required: true
-    },
-    pfp: {
-      type: String,
-      default: () => 
-        'https://api.dicebear.com/6.x/bottts/png'+`?seed=${Date.now()}&eyes=shade01`+
-        '&mouth=smile01&texture=circuits'+'&face=round02&sides=square'
-    },
-    referralId: {
-      type: String,
-      default: () => randomBytes(
-        parseInt(useRuntimeConfig().REFERRAL_ID_BYTES)
-      ).toString('hex'),
-      unique: true
-    },
-    active: {
-      type: Boolean,
-      default: false,
-    },
-    verified: {
-      type: Boolean,
-      default: false,
-    },
-    courseType: {
-      type: String,
-      required: true
-    },
-
-    personalInfo: {
-      pancardNo: { type: String, default: null },
-      phoneNumber: { type: Number, default: null },
+    info: {
+      name: {
+        type: String,
+        required: true,
+        trim: true
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+      },
+      phoneNumber: { type: String, default: null },
       country: { type: String, default: null },
       postalCode: { type: String, default: null },
       cityState: { type: String, default: null },
       streetAddress: { type: String, default: null },
-      bankAccountNo: { type: Number, default: null },
-      ifsc: { type: String, default: null }
+      xlmWalletAddress: { type: String, default: null },
+      pancardNo: { type: String, default: null },
+      bankAccountNo: { type: String, default: null },
+      ifsc: { type: String, default: null },
+    },
+    password: { type: String, required: true },
+    pfp: {
+      type: String,
+      default: () =>
+        'https://api.dicebear.com/6.x/bottts/png'+`?seed=${Date.now()}&eyes=shade01`+
+        '&mouth=smile01&texture=circuits'+'&face=round02&sides=square'
+    },
+    role: { type: String, default: 'member' },
+
+    active: { type: Boolean, default: false, },
+    verified: { type: Boolean, default: false, },
+    courseType: { type: String, required: true },
+    referralId: {
+      type: String,
+      unique: true,
+      default: () => randomBytes(
+        parseInt(useRuntimeConfig().REFERRAL_ID_BYTES)
+      ).toString('hex')
     },
 
-    rank: {
-      type: Number,
-      default: 1,
-    },
+    rank: { type: Number, default: 1, },
     ancestors: [{
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Users'
@@ -79,7 +67,6 @@ const userSchema = new mongoose.Schema({
         },
       }]
     }]
-
   },
   {
     timestamps: true,
@@ -87,7 +74,6 @@ const userSchema = new mongoose.Schema({
   }
 )
 
-// userSchema.pre('')
 
 
 export default mongoose.model(collectionName, userSchema);
