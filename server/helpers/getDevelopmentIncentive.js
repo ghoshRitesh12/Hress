c3369dc083495a6fe00013fcfe5df4acfb4b4083
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-07-10 12:11:26 +0530

    feat: added dev incentive getter

---
+++ server/helpers/getDevelopmentIncentive.js
@@ -0,0 +1,27 @@
+
+/**
+ * @param {number} rank
+ * @returns {number} developmentIncentive
+ */
+const getDevelopmentIncentive = (rank) => {
+  rank = parseInt(rank);
+  if(!rank) throw new Error('rank not found')
+
+  switch(rank) {
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+      return 1;
+    case 11:
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+      return 0.5;
+    default: 
+      return 0;
+  }
+}
+
+export default getDevelopmentIncentive
