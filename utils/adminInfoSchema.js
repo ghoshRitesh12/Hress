e9baddbf9828e880a8e73c5c54f1fa06b18f3e34
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-07-10 11:32:10 +0530

    feat: added admin editable form schema

---
+++ utils/adminInfoSchema.js
@@ -0,0 +1,44 @@
+import { toTypedSchema } from '@vee-validate/yup';
+import { object, string } from 'yup';
+
+
+export const serverAdminInfoSchema = object({
+  name: string().trim().min(3, 'Invalid name'),
+  email: string().trim().email('Invalid email address'),
+  pancardNo: string().trim().matches(
+    /^(?:[A-Z]{5}[0-9]{4}[A-Z])?$/,
+    'Invalid pancard number'
+  ),
+  bankAccountNo: string().trim().matches(
+    /^(?:[0-9]{6,})?$/,
+    'Invalid bank a/c number'
+  ),
+  ifsc: string().trim().matches(
+    /^(?:[A-Z]{4}[0][A-Z0-9]{6})?$/,
+    'Invalid IFSC code'
+  ),
+  phoneNumber: string().trim().matches(
+    /^(?:\d{10})?$/,
+    'Invalid phone number'
+  ),
+  xlmWalletAddress: string().trim().matches(
+    /^(?:[a-zA-Z0-9]{55})?$/, 
+    'Invalid wallet address'
+  ),
+  country: string().trim().matches(
+    /^(?:[A-Za-z\s.'-]{2,})?$/, 
+    'Invalid country name'
+  ),
+  cityState: string().trim().matches(
+    /^$|^[A-Za-z\s.'-]+,[A-Za-z\s.'-]+$/, 
+    'Invalid city, state format'
+  ),
+  postalCode: string().trim().matches(
+    /^(?:\d{6})?$/, 
+    'Invalid postal code'
+  ),
+  streetAddress: string().trim()
+})
+
+
+export const clientAdminInfoSchema = toTypedSchema(serverAdminInfoSchema);
