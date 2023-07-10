94fc1a29c7955149c4587d8d263fd25a0642c21f
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-07-10 11:31:31 +0530

    feat: added any time editable form schema

---
+++ utils/anyTimeInfoSchema.js
@@ -0,0 +1,30 @@
+import { toTypedSchema } from '@vee-validate/yup';
+import { object, string } from 'yup';
+
+
+export const serverAnyTimeInfoSchema = object({
+  phoneNumber: string().trim().matches(
+    /^(?:\d{10})?$/,
+    'Invalid phone number'
+  ),
+  xlmWalletAddress: string().trim().matches(
+    /^(?:[a-zA-Z0-9]{55})?$/, 
+    'Invalid wallet address'
+  ),
+  country: string().trim().matches(
+    /^(?:[A-Za-z\s.'-]{2,})?$/, 
+    'Invalid country name'
+  ),
+  cityState: string().trim().matches(
+    /^$|^[A-Za-z\s.'-]+,[A-Za-z\s.'-]+$/, 
+    'Invalid city, state format'
+  ),
+  postalCode: string().trim().matches(
+    /^(?:\d{6})?$/, 
+    'Invalid postal code'
+  ),
+  streetAddress: string().trim()
+})
+
+
+export const clientAnyTimeInfoSchema = toTypedSchema(serverAnyTimeInfoSchema);
