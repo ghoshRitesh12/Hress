10de55362320559d9e5d7ff3ca5286a64c7e91b1
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-07-12 12:19:28 +0530

    feat: added login request handler

--- server/api/auth/login.post.js
+++ server/api/auth/login.post.js
@@ -1,6 +1,59 @@
 import User from "../../models/User.model";
+import { compare } from 'bcrypt';
+import { serverLoginSchema } from "~/utils/loginSchema";
 
+export default eventHandler(async (event) => {
+  const queryFields = [
+    'info.name', 'info.email', 'pfp',
+    'role', 'verified', 'password'
+  ];
+  try {
+    const body = await readBody(event);
+    console.log(body)
+    await serverLoginSchema.validate(body);
 
-export default eventHandler(async event => {
-  
+    console.log('login endpoint hit');
+
+    const foundUser = await User.findOne({ 'info.email': body.email }, queryFields)
+      .readConcern('majority')
+    if(!foundUser) {
+      return sendError(event, createError({
+        statusCode: 404,
+        statusMessage: 'Invalid credentials.'
+      }))
+    }
+
+    if(!foundUser.verified) {
+      return sendError(event, createError({
+        statusCode: 403,
+        statusMessage: 'Account not verified, try signing up again'
+      }))
+    }
+
+    if(!await compare(body.password, foundUser.password)) {
+      return sendError(event, createError({
+        statusCode: 401,
+        statusMessage: 'Invalid credentials'
+      }))
+    }
+
+    console.log('success response');
+    setResponseStatus(event, 200)
+    return {
+      user: {
+        name: foundUser.info.name,
+        email: foundUser.info.email,
+        image: foundUser.pfp,
+        role: foundUser.role
+      }
+    }
+
+    
+  } catch (err) {
+    console.log(err);
+    return sendError(event, createError({
+      statusCode: err?.statusCode || 500,
+      statusMessage: err?.statusMessage || 'Something Went Wrong',
+    }))
+  }
 })
