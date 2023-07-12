2054fc57796b5fe776362e714b4cebce5086ad2d
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-07-12 12:01:11 +0530

    feat: added income page

--- pages/account/income.vue
+++ pages/account/income.vue
@@ -1,7 +1,50 @@
 <template>
 
-  <div>
-    Income page
+  <div data-account-income>
+
+    <div v-if="data?.userActive">
+
+      <div 
+        class="
+        text-2xl border-b-[1px] border-zinc-700/80
+        pb-3 mt-0
+        "
+      >
+        My Income
+      </div>
+  
+      <IncomeLevelWiseTab 
+        :levels="data?.levels"
+        :total-level-income="data?.totalLevelIncome"
+        class="my-6"
+      />
+  
+      <IncomeDevIncentiveTab
+        class="my-6"
+        endpoint="/api/account/income/dev-incentive"
+      />
+  
+      <IncomeTrainerIncentive
+        :trainer-incentive="data?.trainerIncentive"
+      />
+      
+    </div>
+
+
+    <Inconvenience v-else
+      img-src="/images/inactive.svg"
+      img-style="max-w-[15rem] md:max-w-[22rem]"
+      class="mt-10 md:mt-5"
+    >
+      It seems like your account is not active,
+      because it was 
+      <strong>not approved</strong>  
+      by any admin.
+      <br>
+      Activate your account in order to continue
+      your daily activities.
+    </Inconvenience>
+
   </div>
 
 </template>
@@ -14,8 +57,16 @@ useHead({
 })
 
 definePageMeta({
-  layout: 'account'
+  layout: 'account',
+  middleware: 'native'
 })
+
+
+const { setPopupMessage } = usePopup();
+
+const { data, error } = await useFetch('/api/account/income');
+if(error.value) setPopupMessage(error.value?.statusMessage);
+
   
 </script>
 
