faa2cc7b2000db98dca88f75b9daeb566949b82b
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-07-12 12:14:49 +0530

    feat: added admin viewable profile about page

---
+++ pages/admin/profile/[referralId]/about.vue
@@ -0,0 +1,99 @@
+<template>
+
+  <div data-profile>
+
+    <div
+      class="
+      flex items-center gap-10
+      border-[0px] border-zinc-600
+      rounded-2xl mb-10 flex-wrap lg:flex-nowrap
+      justify-center lg:justify-normal
+      "
+    >
+      <AccountProfileHeader
+        :username="profile?.info?.name"
+        :pfp="profile?.pfp"
+      />
+
+      <div
+        class="
+        flex flex-col gap-6 text-[1.05rem]
+        w-full items-center
+        "
+      >
+        <AccountCourse
+          :course-type="profile?.courseType"
+        />
+        <AccountVerifiedTab
+          class="w-full"
+          :verified="profile?.verified"
+        />
+        <AccountActiveTab
+          class="w-full"
+          :is-active="profile?.active"
+        />
+      </div>
+      
+    </div>
+
+
+    <div v-if="params?.referralId?.trim()"
+      class="
+      flex items-center gap-x-10 gap-y-4 flex-wrap 
+      lg:flex-nowrap my-8
+      "
+    >
+      <AdminAccountActivateForm/>
+      <AdminAccountCourseForm/>
+    </div>
+
+
+    <div
+      class="
+      flex gap-x-10 gap-y-4 flex-wrap 
+      lg:flex-nowrap my-8
+      "
+    >
+      <AccountRank 
+        :rank="profile?.rank"
+      />
+      <AccountReferralId
+        :referral-id="profile?.referralId"
+      />
+    </div>
+
+    <AccountPersonalInfoSection
+      :info="profile?.info"
+    />
+
+    <AccountInfoEditModal/>
+
+    <Popup/>
+
+  </div>
+
+</template>
+
+
+<script setup>
+
+const { params } = useRoute();
+
+useHead({
+  title: `${params.referralId} - About`,
+})
+definePageMeta({
+  layout: 'view-profile',
+  middleware: ['native', 'admin']
+})
+
+
+const { profile, fetchProfile } = useProfile();
+await fetchProfile(`/api/admin/profile/${params.referralId}/about`)
+
+</script>
+
+
+<style scoped>
+
+</style>
