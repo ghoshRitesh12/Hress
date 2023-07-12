92caf6a58d50a9143d375d8d77abd8bf22b352ac
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-07-12 11:25:32 +0530

    feat: added modal opening btn & style changes

--- components/account/PersonalInfoSection.vue
+++ components/account/PersonalInfoSection.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <div
+  <div v-if="props.info"
     class="
     py-4 px-5 border-[1px] 
     border-zinc-600 rounded-2xl
@@ -9,51 +9,50 @@
 
     <div 
       class="
-      flex items-center justify-between 
-      pb-4 mb-8
+      flex items-center pb-4 mb-8
       "
     >
 
-      <div class="text-xl font-semibold">
+      <div 
+        class="
+        text-lg sm:text-xl flex-[65%]
+        flex-grow-0 font-semibold
+        "
+      >
         Personal Information
       </div>
 
-      <button 
-        class="
-        flex items-center bg-zinc-800 w-fit
-        py-2 px-4 rounded-xl text-zinc-300
-        hover:bg-zinc-700/80 transition
-        ease-in duration-100 ml-6
-        "
-      >
-
-        <div>Edit</div>
-
-        <Icon
-          class="ml-2 text-lg flex-shrink-0"
-          name="iconamoon:edit"
-        />
-
-      </button>
+      <div class="ml-auto">
+        <button 
+          class="
+          flex items-center bg-zinc-800 w-fit
+          py-2 px-4 rounded-xl text-zinc-300
+          hover:bg-zinc-700/90 transition
+          ease-in duration-100
+          "
+          @click="openInfoEditModal"
+        >
+          <div>Edit</div>
+          <Icon
+            class="ml-2 text-lg flex-shrink-0"
+            name="iconamoon:edit"
+          />
+        </button>
+      </div>
 
     </div>
 
 
     <section
-      class="
-      grid gap-y-8 gap-x-4 info-section
-      "
+      class="grid gap-y-8 gap-x-4 info-section"
       style="grid-template-columns: repeat(var(--col), 1fr);"
     >
-
-
       <AccountPersonalInfo
         v-for="value, key in info"
+        :key="key"
         :context="infoMap[key]"
         :context-value="value"
       />
-
-
     </section>
 
 
@@ -64,19 +63,19 @@
 
 <script setup>
 
+const { openInfoEditModal } = useInfoEdit();
+
 const props = defineProps({
   info: {
     type: Object,
-    required: true
   }
 })
-
 
 const infoMap = {
   name: "Full name",
   email: "Email address",
   pancardNo: "Pancard No",
-  phoneNumber: "Phone",
+  phoneNumber: "Phone Number",
   country: "Country",
   postalCode: "Postal Code",
   cityState: "City, State",
@@ -95,7 +94,7 @@ const infoMap = {
     --col: 1;
   }
 
-  @media (min-width: 786px) {
+  @media (min-width: 640px) {
     .info-section {
       --col: 2;
     }
