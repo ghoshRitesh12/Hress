0c80d21096dbd4c8427a81554a45d93bfa9e71c0
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-07-12 11:47:13 +0530

    feat: added member rank component

---
+++ components/team/MemberRank.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <div 
+    class="whitespace-nowrap text-[.85rem]"
+  >
+    {{ getRankName(props.rank) }}
+  </div>
+
+</template>
+
+
+<script setup>
+import getRankName from '~/utils/getRankName';
+
+const props = defineProps({
+  rank: {
+    type: Number,
+    required: true
+  }
+})
+
+</script>
+
+
+<style>
+
+</style>
