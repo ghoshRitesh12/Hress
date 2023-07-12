88fbe25355f8d6dc9616fe3a478f84490fdc1eff
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-07-12 11:39:31 +0530

    feat: added trainer incentive component

---
+++ components/income/TrainerIncentive.vue
@@ -0,0 +1,45 @@
+<template>
+  
+  <div data-trainer-incentive
+    class="
+    flex items-center justify-between
+    bg-zinc-900 rounded-2xl
+    px-4 py-4 text-[1rem]
+    "
+  >
+
+    <div 
+      class="pointer-events-none"
+    >
+      Trainer Incentive
+    </div>
+
+    <div class="text-[.95rem]">
+      {{  
+        new Intl.NumberFormat(
+        'en-IN', 
+        { style: 'currency', currency: 'INR' }
+        ).format(props.trainerIncentive || 0)
+      }}
+    </div>
+
+  </div>
+  
+</template>
+
+
+<script setup>
+
+const props = defineProps({
+  trainerIncentive: {
+    type: Number,
+    default: 0
+  }
+})
+
+</script>
+
+
+<style scoped>
+
+</style>
