a30a9788902f5558c2fad0e57a9db2d1597242ea
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-07-14 00:03:15 +0530

    refactor: login form schema

--- utils/loginSchema.js
+++ utils/loginSchema.js
@@ -2,21 +2,13 @@ import { toTypedSchema } from '@vee-validate/yup';
 import { object, string } from 'yup';
 
 
-export const clientLoginSchema = toTypedSchema(object({
-  email: string().trim().required('Email id is required').email('invalid email address'),
-  // password: string().trim().required().matches(
-  //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*()])(?=.{8,16}$)/,
-  //   'Use 8 to 16 characters with a mix of letters(lower & upper case), numbers & symbols among !@#$%^&*()'
-  // ),
-  password: string().trim().required()
-}))
-
 export const serverLoginSchema = object({
-  email: string().trim().required('Email id is required').email('invalid email address'),
-  // password: string().trim().required().matches(
-  //   /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*()])(?=.{8,16}$)/,
-  //   'Use 8 to 16 characters with a mix of letters(lower & upper case), numbers & symbols among !@#$%^&*()'
-  // ),
-  password: string().trim().required()
+  email: string().trim().required('Email id is required').email('Invalid email address'),
+  password: string().trim().required('Password required').matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*()])(?=.{8,16}$)/,
+    'Use 8 to 16 characters with a mix of letters(lower & upper case), numbers & symbols among !@#$%^&*()'
+  ),
 })
+
+export const clientLoginSchema = toTypedSchema(serverLoginSchema)
 
