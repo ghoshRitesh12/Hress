c32f2913ba9ee712aa0c63e643ee14cddb471cc0
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-07-13 19:36:46 +0530

    perf: reduced no of queries

--- server/api/account/team.get.js
+++ server/api/account/team.get.js
@@ -5,39 +5,45 @@ export default eventHandler(async (event) => {
   try {
     await nativeAuthenticate(event);
 
-    const queryFields = [
-      'ancestors', 'levels', 'active'
-    ];
+    const queryFields = ['ancestors', 'levels', 'active'];
     const res = { 
-      sponsorer: null, levels: [],
+      sponsorer: null, 
+      levels: [],
       userActive: false
+    }
+
+    const teamPopulateFields = ['info.name', 'rank', 'active']
+    if(event?.user?.role === 'admin') {
+      teamPopulateFields.push('referralId');
     }
 
     const foundUser = await User.findOne({ 'info.email': event?.user?.email }, queryFields)
       .readConcern('majority')
+      .populate({
+        path: 'levels.referrals.userRef',
+        select: teamPopulateFields
+      }).slice('ancestors', -1)
+      .populate({ 
+        path: 'ancestors',
+        select: ['info.name', 'referralId'] 
+      })
+
     if(!foundUser) {
       return sendError(event, createError({
         statusCode: 404,
         statusMessage: 'User not found'
       }))
     }
-
+    
     res.userActive = foundUser.active
     if(!res.userActive) return res;
 
-    res.levels = (await foundUser.populate({ 
-      path: 'levels.referrals.userRef',
-      select: ['info.name', 'rank'],
-    })).levels
-
-    res.sponsorer = (await foundUser.populate({ 
-      path: 'ancestors',
-      select: ['info.name', 'referralId'] 
-    })).ancestors.at(-1) || null
-
+    res.levels = foundUser.levels
+    res.sponsorer = foundUser.ancestors[0];
 
     setResponseStatus(event, 200)
     return res
+
 
   } catch (err) {
     console.log(err);
