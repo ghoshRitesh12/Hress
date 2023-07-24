1aeac76eedc055e7cbf7b7581872c9640d6ddcb1
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-07-25 00:15:05 +0530

    build: added prerender of static pages & updated route-rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    "@nuxtjs/tailwindcss", "@nuxt/image-edge", 
-    "nuxt-icon", "@nuxt/content", "nuxt-simple-sitemap",
-    "nuxt-simple-robots", "@vee-validate/nuxt",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/image-edge",
+    "nuxt-icon",
+    "@nuxt/content",
+    "nuxt-simple-sitemap",
+    "nuxt-simple-robots",
+    "@vee-validate/nuxt",
     "@sidebase/nuxt-auth",
   ],
   runtimeConfig: {
@@ -22,51 +26,54 @@ export default defineNuxtConfig({
     indexable: true,
     public: {
       siteUrl: process.env.HRESS_BASE_URL,
-    }
+    },
   },
   nitro: {
-    plugins: ["./db/index.js"]
+    plugins: ["./db/index.js"],
   },
   veeValidate: {
     autoImports: true,
     componentNames: {
-      Form: 'VForm',
-      Field: 'VField',
-      ErrorMessage: 'VErrorMessage'
-    }
+      Form: "VForm",
+      Field: "VField",
+      ErrorMessage: "VErrorMessage",
+    },
   },
   auth: {
-    origin: process.env.NODE_ENV === 'development' ? 
-      process.env.AUTH_ORIGIN_DEV : process.env.AUTH_ORIGIN_PROD,
+    origin:
+      process.env.NODE_ENV === "development"
+        ? process.env.AUTH_ORIGIN_DEV
+        : process.env.AUTH_ORIGIN_PROD,
     isEnabled: true,
-    defaultProvider: 'credentials',
+    defaultProvider: "credentials",
     enableGlobalAppMiddleware: false,
   },
   app: {
     head: {
-      title: 'Hress',
-      htmlAttrs: { lang: 'en', dir: 'ltr', },
+      title: "Hress",
+      htmlAttrs: { lang: "en", dir: "ltr" },
       meta: [
         {
-          name: 'content-language',
-          content: 'en'
+          name: "content-language",
+          content: "en",
         },
         {
-          name: 'viewport',
-          content: 'width=device-width, initial-scale=1, user-scalable=no'
-        }
-      ]
+          name: "viewport",
+          content: "width=device-width, initial-scale=1, user-scalable=no",
+        },
+      ],
     },
   },
   routeRules: {
     "/": { prerender: true },
-    "/plan": { static: true },
+    "/plan": { prerender: true },
     "/terms": { prerender: true },
-    "/privacy": { static: true },
-    "/cookie-policy": { static: true },
-    "/account/**": { ssr: false }
+    "/privacy": { prerender: true },
+    "/cookie-policy": { prerender: true },
+    "/account/**": { ssr: false },
+    "/admin/**": { ssr: false },
   },
   robots: {
-    disallow: ['/api', '/account'],
+    disallow: ["/api", "/account", "/admin"],
   },
-})
+});
