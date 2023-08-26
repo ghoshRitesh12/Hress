3183157a1f479334ce49846e7009b2df33c32c7a
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-08-27 01:15:31 +0530

    feat: added `active-token` page

---
+++ pages/account/active-token.vue
@@ -0,0 +1,25 @@
+<template>
+  
+  <div data-account-active-token>
+
+    <ActiveTokenExisting />
+
+    <ActiveTokenGenerate />
+
+    <Popup />
+
+  </div>
+  
+</template>
+
+
+<script setup>
+useHead({
+  title: 'Account - Active Token'
+})
+definePageMeta({
+  layout: 'account',
+  middleware: ['native', 'admin']
+})
+
+</script>
