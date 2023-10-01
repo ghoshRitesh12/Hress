71f9fa1e8ec615f60446ce2bb9ecf5eccf00a7e2
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-10-01 09:57:39 +0530

    feat: update new rank names

--- utils/getRankName.js
+++ utils/getRankName.js
@@ -2,21 +2,21 @@ const getRankName = (rank = 1) => {
   rank = rank ? rank : 1
   const rankNames = [
     "N/A",
-    "One (1)",
-    "Two (2)",
-    "Three (3)",
-    "Four (4)",
-    "Star Trader (5)",
-    "Silver Trader (6)",
-    "Gold Trader (7)",
-    "Platinum Trader (8)",
+    "One(1)",
+    "Runner (2)",
+    "Bronze Trader (3)",
+    "Silver Trader (4)",
+    "Gold Trader (5)",
+    "Platinum Trader (6)",
+    "Emerald Trader (7)",
+    "Saphire Trader (8)",
     "Ruby Trader (9)",
     "Diamond Trader (10)",
-    "Blue Diamond Trader (11)",
-    "Gold Diamond Trader (12)",
-    "Royal Diamond Trader (13)",
-    "Crown Diamond Trader (14)",
-    "Crown Ambassador (15)"
+    "Black Diamond Trader (11)",
+    "Blue Diamond Trader (12)",
+    "Red Diamond Trader (13)",
+    "Crown Trader (14)",
+    "Crown Ambassador (15)",
   ]
 
   return (rank < 1 || rank > 15) ? rankNames[0] : rankNames[rank]
