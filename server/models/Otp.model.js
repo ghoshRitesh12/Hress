3bf004f837a35c04f6ed0797423fafc494aa5957
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-10-01 09:59:09 +0530

    feat: update otp model

--- server/models/Otp.model.js
+++ server/models/Otp.model.js
@@ -1,32 +1,29 @@
-import { Schema, model } from 'mongoose';
-import { randomInt } from 'crypto';
+import { Schema, model } from "mongoose";
+import { randomInt } from "crypto";
 
-const collectionName = 'Otp';
+const collectionName = "Otp";
 
 const otpSchema = new Schema({
-    email: {
-      type: String,
-      required: true,
-    },
-    otp: {
-      type: Number,
-      default: () => randomInt(100000, 999999),
-    },
-    validMins: {
-      type: Number,
-      default: 5,
-    },
-    expireAt: {
-      type: Date,
-      default: Date.now,
-      expires: 300
-    },
+  email: {
+    type: String,
+    index: true,
+    required: true,
   },
-  { 
+  otp: {
+    type: Number,
+    default: () => randomInt(100000, 999999)
+  },
+  expireAt: {
+    type: Date,
+    default: Date.now,
+    expires: 300 // 5 mins
+  },
+},
+  {
     collection: collectionName,
     writeConcern: {
-      w: 'majority',
-      j: true
+      w: "majority",
+      journal: true,
     }
   }
 )
