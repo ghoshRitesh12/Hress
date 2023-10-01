3e3a7f2a585a57cb069658f5636481e6d1d777d2
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-10-01 09:58:39 +0530

    feat: update `writeConcern`'s journal

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -1,83 +1,92 @@
-import mongoose from "mongoose";
+import { Schema, model } from "mongoose";
 import { randomBytes } from "crypto";
 
-const collectionName = 'Users';
+const collectionName = "Users";
 
-const userSchema = new mongoose.Schema({
-    info: {
-      name: {
-        type: String,
-        required: true,
-        trim: true
-      },
-      email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-      },
-      phoneNumber: { type: String, default: null },
-      country: { type: String, default: null },
-      postalCode: { type: String, default: null },
-      cityState: { type: String, default: null },
-      streetAddress: { type: String, default: null },
-      xlmWalletAddress: { type: String, default: null },
-      pancardNo: { type: String, default: null },
-      bankAccountNo: { type: String, default: null },
-      ifsc: { type: String, default: null },
-    },
-    password: { type: String, required: true },
-    pfp: {
-      type: String,
-      default: () =>
-        'https://api.dicebear.com/6.x/bottts/png'+`?seed=${Date.now()}&eyes=shade01`+
-        '&mouth=smile01&texture=circuits'+'&face=round02&sides=square'
-    },
-    role: { type: String, default: 'member' },
+// set('debug', true);
 
-    active: { type: Boolean, default: false, },
-    verified: { type: Boolean, default: false, },
-    courseType: { type: String, required: true },
-    referralId: {
+const userSchema = new Schema({
+  info: {
+    name: {
       type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
       unique: true,
-      default: () => randomBytes(
-        parseInt(useRuntimeConfig().REFERRAL_ID_BYTES)
-      ).toString('hex')
+      trim: true,
+      index: true,
     },
-
-    rank: { type: Number, default: 1, },
-    ancestors: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Users'
-    }],
-    levels: [{
-      levelNo: {
-        type: Number,
-        default: 0
-      },
-      referrals: [{
-        commission: {
-          type: Number,
-          default: 0,
-        },
-        userRef: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: 'Users'
-        },
-      }]
-    }]
+    phoneNumber: { type: String, default: null },
+    country: { type: String, default: null },
+    postalCode: { type: String, default: null },
+    cityState: { type: String, default: null },
+    streetAddress: { type: String, default: null },
+    xlmWalletAddress: { type: String, default: null },
+    pancardNo: { type: String, default: null },
+    bankAccountNo: { type: String, default: null },
+    ifsc: { type: String, default: null },
   },
+  password: { type: String, required: true },
+  pfp: {
+    type: String,
+    default: () =>
+      'https://api.dicebear.com/7.x/bottts/png' + `?seed=${Date.now()}&eyes=shade01` +
+      '&mouth=smile01&texture=circuits' + '&face=round02&sides=square'
+  },
+
+  role: { type: String, default: "member" },
+  active: { type: Boolean, default: false, },
+  verified: { type: Boolean, default: false, },
+  courseType: { type: String, required: true },
+
+  rank: { type: Number, default: 1, },
+  referralId: {
+    type: String,
+    index: true,
+    unique: true,
+    default: () => randomBytes(
+      Number(useRuntimeConfig().REFERRAL_ID_BYTES)
+    ).toString('hex')
+  },
+
+  ancestors: [{
+    type: Schema.Types.ObjectId,
+    ref: "Users"
+  }],
+  levels: [{
+    levelNo: {
+      type: Number,
+      default: 0
+    },
+    referrals: [{
+      commission: {
+        type: Number,
+        default: 0,
+      },
+      createdAt: {
+        type: String, // day-month-year
+        default: null
+      },
+      userRef: {
+        type: Schema.Types.ObjectId,
+        ref: "Users",
+        required: true,
+      },
+    }]
+  }],
+},
   {
     timestamps: true,
     collection: collectionName,
     writeConcern: {
-      w: 'majority',
-      j: true
+      w: "majority",
+      journal: true,
     }
   }
 )
 
 
-
-export default mongoose.model(collectionName, userSchema);
+export default model(collectionName, userSchema);
