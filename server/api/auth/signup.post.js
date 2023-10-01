73c375b5d28be310a5562037ac78c89ca385d2e4
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-10-01 10:14:29 +0530

    feat: add `Income` profile creation

--- server/api/auth/signup.post.js
+++ server/api/auth/signup.post.js
@@ -1,5 +1,6 @@
 import Otp from "../../models/Otp.model";
 import User from "../../models/User.model";
+import Income from "~/server/models/Income.model";
 import ActiveToken from "~/server/models/ActiveToken.model";
 import { hash } from "bcrypt";
 import { startSession } from "mongoose";
@@ -9,118 +10,126 @@ import { getIncentive, isRankValid } from "../../helpers/utils.js";
 export default eventHandler(async (event) => {
   const signupSession = await startSession();
   signupSession.startTransaction({
-    readConcern: 'snapshot',
+    readConcern: "snapshot",
     writeConcern: {
-      w: 'majority', j: true
+      w: "majority",
+      journal: true
     }
   })
 
   try {
     const body = await readBody(event);
-    await serverSignupSchema.validate(body);    
+    await serverSignupSchema.validate(body);
 
     const tempUser = await Otp.findOne({ email: body.email }).session(signupSession);
-    if(!tempUser) {
+    if (!tempUser) {
       await signupSession.abortTransaction();
       return sendError(event, createError({
-        statusCode: 401, 
-        statusMessage: 'Otp has expired, try 10 mins later',
+        statusCode: 401,
+        statusMessage: "Otp has expired, try 10 mins later",
       }))
     }
-    if(tempUser.otp !== Number(body.otp)) {
+    if (tempUser.otp !== Number(body.otp)) {
       await signupSession.abortTransaction();
       return sendError(event, createError({
-        statusCode: 403, 
-        statusMessage: 'Invalid OTP, try 10 mins later',
+        statusCode: 403,
+        statusMessage: "Invalid OTP, try again",
       }))
     }
-    await Otp.deleteOne({ email: body.email });
+    await Otp.deleteOne({ email: body.email }).session(signupSession);
 
-    
 
-    if(await User.exists({ 'info.email': body.email }).session(signupSession)) {
+
+    if (await User.exists({ 'info.email': body.email }).session(signupSession)) {
       await signupSession.abortTransaction();
       return sendError(event, createError({
-        statusCode: 409, 
-        statusMessage: 'User already exists',
+        statusCode: 409,
+        statusMessage: "User already exists",
       }))
     }
 
     const spillOverIncentive = 20;
-    const sponsorerQueryFields = ['active', 'ancestors', 'referralId']
-    const sponsorer = await User.findOne({ referralId: body.sponsorerId }, sponsorerQueryFields)
-      .session(signupSession);
-    if(!sponsorer) {
+    const sponsorerQueryFields = {
+      _id: 1,
+      active: 1,
+      ancestors: 1,
+      referralId: 1
+    }
+    const sponsorer = await User.findOne({ referralId: body.sponsorerId })
+      .session(signupSession)
+      .select(sponsorerQueryFields)
+    if (!sponsorer) {
       await signupSession.abortTransaction();
       return createError({
         statusCode: 404,
-        statusMessage: 'Sponsorer not found'
+        statusMessage: "Sponsorer not found"
       })
     }
-    if(!sponsorer.active) {
+    if (!sponsorer.active) {
       await signupSession.abortTransaction();
       return createError({
         statusCode: 403,
-        statusMessage: 'Sponsorer account inactive'
+        statusMessage: "Sponsorer account inactive"
       })
     }
 
 
-    if(body.refererId) {
-      const refererQueryFields = ['active', 'levels']
-      const referer = await User.findOne({ referralId: body.refererId }, refererQueryFields).session(signupSession);
-      if(!referer) {
+    if (body?.refererId) {
+      const referer = await User.findOne({ referralId: body.refererId })
+        .session(signupSession)
+        .select({ _id: 1, active: 1 });
+
+      if (!referer) {
         await signupSession.abortTransaction();
         return sendError(event, createError({
-          statusCode: 400, 
-          statusMessage: 'Referer not found',
+          statusCode: 400,
+          statusMessage: "Referer not found",
         }))
       }
-      if(!referer.active) {
+      if (!referer.active) {
         await signupSession.abortTransaction();
         return createError({
           statusCode: 403,
-          statusMessage: 'Referer account inactive'
+          statusMessage: "Referer account inactive"
         })
       }
 
-      const sponsorerAncestors = sponsorer.ancestors.map(i => `${i._id}`);
+      const sponsorerAncestors = sponsorer.ancestors.map(a => `${a._id}`);
       const sponsorersRefererIndex = sponsorerAncestors.indexOf(`${referer._id}`)
-      if(sponsorersRefererIndex === -1) {
+      if (sponsorersRefererIndex === -1) {
         await signupSession.abortTransaction();
         return createError({
-          statusCode: 400,
-          statusMessage: 'Referer not related to sponsorer'
+          statusCode: 403,
+          statusMessage: "Referer not related to sponsorer"
         })
       }
 
       const refererIndexedLevel = sponsorerAncestors.length - sponsorersRefererIndex;
-      if(refererIndexedLevel >= 15) {
+      if (refererIndexedLevel >= 12) {
         await signupSession.abortTransaction();
         return createError({
-          statusCode: 400,
-          statusMessage: 'Referer reached max spillover level'
+          statusCode: 409,
+          statusMessage: "Referer reached max spillover level"
         })
       }
     }
 
     let isAccountActive = false;
     let foundActiveToken = null;
-    if(body.activeToken) {
-      foundActiveToken = await ActiveToken.findOne(
-        { token: body.activeToken }, 
-        ['usedBy']
-      ).session(signupSession) || null;
+    if (body?.activeToken) {
+      foundActiveToken = await ActiveToken.findOne({ token: body.activeToken })
+        .session(signupSession)
+        .select({ usedBy: 1 }) || null;
 
       // used active token
-      if(foundActiveToken?.usedBy) {
+      if (foundActiveToken?.usedBy) {
         await signupSession.abortTransaction();
         return createError({
           statusCode: 403,
           statusMessage: "Your active token has expired"
         })
       }
-  
+
       isAccountActive = foundActiveToken ? true : false;
     }
 
@@ -130,66 +139,89 @@ export default eventHandler(async (event) => {
         name: body.fullname,
         email: body.email,
       },
-      courseType: body.course,
-      verified: true,
-      active: isAccountActive,
       password: await hash(
         body.password, parseInt(useRuntimeConfig().PWD_SALT)
       ),
+      verified: true,
+      active: isAccountActive,
+      courseType: body.course,
       ancestors: [...sponsorer.ancestors, `${sponsorer._id}`].sort()
     }]
     const newUser = (await User.create(newUserInfo, { session: signupSession }))[0];
 
-    
+
     // setting active token usedBy
-    if(foundActiveToken) {
+    if (foundActiveToken) {
       foundActiveToken.usedBy = newUser.referralId
       await foundActiveToken.save({ session: signupSession })
     }
 
+    await Income.create(
+      [{
+        email: newUser.info.email,
+        referralId: newUser.referralId,
+        userProfile: newUser._id,
+      }],
+      { session: signupSession }
+    )
+
     // for each ancestor
-    const ancestorsQueryFields = ['referralId', 'role', 'levels', 'rank'];
-    const ancestors = await User.find({ _id: { $in: newUser.ancestors } }, ancestorsQueryFields).session(signupSession);
+    const ancestorsQueryFields = {
+      _id: 1,
+      role: 1,
+      rank: 1,
+      levels: 1,
+      referralId: 1,
+    };
+
+    // ancestorIds -> newUser.ancestors 
+    const ancestors = await User.find({ _id: { $in: newUser.ancestors } })
+      .session(signupSession)
+      .select(ancestorsQueryFields);
     const sponsorAncestorReferralIds = ancestors.slice(0, -1).map(a => a.referralId);
 
 
     const updateOperations = ancestors.map((ancestor, index) => {
       const indexedLevel = ancestors.length - index;
-      if(ancestor.role !== 'admin' && indexedLevel > 15) return null;
-      
+      if (ancestor.role !== 'admin' && indexedLevel > 15) return null;
+
 
       let incentive = getIncentive(indexedLevel)
       // indirect spillover
-      if(`${ancestor._id}` === `${newUser.ancestors.at(-1)}` && sponsorAncestorReferralIds.includes(body.refererId)) {
+      if (`${ancestor._id}` === `${newUser.ancestors.at(-1)}` && sponsorAncestorReferralIds.includes(body.refererId)) {
         incentive = -1;
       }
       // direct spillover
-      if(ancestor.referralId === body.refererId && body.refererId !== sponsorer.referralId) {
+      if (ancestor.referralId === body.refererId && body.refererId !== sponsorer.referralId) {
         incentive = spillOverIncentive + getIncentive(indexedLevel)
       }
 
-      
+
       // inserting new member
+      const d = new Date();
+      const memberCreatedAt = `${d.getDate() <= 15 ? 15 : 30}-${d.getMonth() + 1}-${d.getFullYear()}`;
       const foundLevelIndex = ancestor.levels.findIndex(level => level.levelNo === indexedLevel);
-      if(foundLevelIndex !== -1) {
+      if (foundLevelIndex !== -1) {
         ancestor.levels[foundLevelIndex].referrals.push({
           commission: incentive,
-          userRef: newUser._id
+          userRef: newUser._id,
+          createdAt: memberCreatedAt,
         })
       } else {
         ancestor.levels.push({
           levelNo: indexedLevel,
           referrals: {
             commission: incentive,
-            userRef: newUser._id
+            userRef: newUser._id,
+            createdAt: memberCreatedAt,
           }
         })
       }
 
       // setting rank
       let tempRank = 1;
-      for(const level of ancestor.levels) {
-        if(!isRankValid(level.levelNo, level.referrals.length)) {
+      for (const level of ancestor.levels) {
+        if (!isRankValid(level.levelNo, level.referrals.length)) {
           break;
         }
         tempRank = level.levelNo === 0 ? 1 : level.levelNo
@@ -201,7 +233,7 @@ export default eventHandler(async (event) => {
         updateOne: {
           filter: { _id: ancestor._id },
           update: {
-            $set: { 
+            $set: {
               levels: ancestor.levels,
               rank: ancestor.rank
             }
@@ -210,23 +242,21 @@ export default eventHandler(async (event) => {
       };
     }).filter(i => i !== null);
 
-
     await User.bulkWrite(updateOperations, { session: signupSession })
     await signupSession.commitTransaction()
 
-
     setResponseStatus(event, 201);
     return {
-      message: 'Account created successfully 🎉'
+      message: "Account created successfully 🎉"
     };
-    
+
   } catch (err) {
     console.log(err);
     await signupSession.abortTransaction();
 
     return sendError(event, createError({
       statusCode: 500,
-      statusMessage: 'Account creation unsuccessful, try again'
+      statusMessage: "Account creation unsuccessful, try again"
     }))
 
   } finally {
