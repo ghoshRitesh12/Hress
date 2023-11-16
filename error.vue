9d7bdf7d2d0cfc40adcfc6215cfa815ac6905532
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-11-17 01:07:08 +0530

    refactor: error page

--- error.vue
+++ error.vue
@@ -1,37 +1,30 @@
 <template>
-  
   <div class="flex flex-col min-h-[100vh]">
-
-    <Titlebar 
+    <Titlebar
       :set-profile="true"
       nav-open-icon="fluent:grid-dots-24-regular"
     />
 
     <div class="px-6 pt-6 sm:pt-8 pb-16">
-      Error {{ props.error.statusMessage }}
-
-      <pre>
-        {{ props.error }}
-      </pre>
+      ERROR -> {{ props?.error?.statusMessage || props?.error?.message }} <br />
+      <br />
+      URL -> {{ props?.error?.url }} <br />
+      <br />
+      STATUS_CODE -> {{ props?.error?.statusCode }} <br />
+      <br />
+      STACK:
+      <div
+        v-html="props?.error?.stack"
+        class="max-w-[50%] text-zinc-400 text-sm"
+      ></div>
     </div>
 
-    <Footer/>
-
+    <Footer />
   </div>
-  
 </template>
 
-
 <script setup>
-
-
-
-const props = defineProps(['error'])
-
-
+const props = defineProps(["error"]);
 </script>
 
-
-<style scoped>
-
-</style>
+<style scoped></style>
