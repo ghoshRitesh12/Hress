53885c152c4d4ea16476bb8c8676ba8539c3cbff
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-11-17 01:26:46 +0530

    feat: added past income statements field

--- server/api/account/income/index.get.js
+++ server/api/account/income/index.get.js
@@ -1,54 +1,121 @@
 import User from "~/server/models/User.model";
+import Income from "~/server/models/Income.model";
 import { nativeAuthenticate } from "~/server/helpers/middleware/native.auth";
 
 export default eventHandler(async (event) => {
   try {
     await nativeAuthenticate(event);
 
-    const foundUser = await User.aggregate([
-      {
-        $match: {
-          'info.email': event?.user?.email
-        }
-      },
-      {
-        $project: {
-          _id: 0,
-          userActive: "$active",
-          selectIncomeLevels: {
-            $cond: {
-              if: { $eq: ["$active", true] },
-              then: {
-                $map: {
-                  input: "$levels",
-                  as: "level",
-                  in: {
-                    label: {
-                      $concat: [
-                        "Level ",
-                        { $toString: "$$level.levelNo" }
-                      ]
-                    },
-                    value: "$$level.levelNo"
+    const [incomeLevelOptions, pastIncomeStatements] = await Promise.all([
+      User.aggregate([
+        {
+          $match: {
+            'info.email': event?.user?.email
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            userActive: "$active",
+            selectIncomeLevels: {
+              $cond: {
+                if: { $eq: ["$active", true] },
+                then: {
+                  $map: {
+                    input: "$levels",
+                    as: "level",
+                    in: {
+                      label: {
+                        $concat: [
+                          "Level ",
+                          { $toString: "$$level.levelNo" }
+                        ]
+                      },
+                      value: "$$level.levelNo"
+                    }
                   }
-                }
-              },
-              else: []
+                },
+                else: []
+              }
             }
           }
-        }
-      },
-    ]).readConcern("majority");
+        },
+      ]).readConcern("majority"),
 
-    if (!foundUser || foundUser?.length === 0) {
-      return sendError(event, createError({
-        statusCode: 404,
-        statusMessage: "User not found"
-      }))
-    }
+      Income.aggregate([
+        {
+          $match: {
+            email: event?.user?.email
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            pastIncomeStatements: {
+              $map: {
+                input: { $objectToArray: "$incomes" },
+                as: "item",
+                in: {
+                  $let: {
+                    vars: {
+                      ogValue: "$$item.k",   //originalValue
+                      ogDate: {              //originalDate
+                        $dateToParts: {
+                          date: { $toDate: "$$item.k" }
+                        }
+                      },
+                      monthsInString: [
+                        "January", "February", "March", "April",
+                        "May", "June", "July", "August",
+                        "September", "October", "November", "December"
+                      ]
+                    },
+                    in: {
+                      label: {
+                        $concat: [
+                          {
+                            $cond: {
+                              if: { $lte: ["$$ogDate.day", 15] },
+                              then: "1st half of",
+                              else: "2nd half of"
+                            }
+                          },
+                          " ",
+                          {
+                            $arrayElemAt: [
+                              "$$monthsInString",
+                              { $subtract: ["$$ogDate.month", 1] }
+                            ]
+                          },
+                          " ",
+                          {
+                            $toString: "$$ogDate.year"
+                          }
+                        ]
+                      },
+                      value: "$$ogValue",
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+      ]).readConcern("majority"),
+    ])
 
-    setResponseStatus(event, 200)
-    return foundUser?.[0] ?? {};
+    // if (!incomeLevelOptions || incomeLevelOptions?.length === 0) {
+    //   return sendError(event, createError({
+    //     statusCode: 404,
+    //     statusMessage: "Income level options not found"
+    //   }))
+    // }
+
+    setResponseStatus(event, 200);
+    return {
+      ...incomeLevelOptions?.[0],
+      ...pastIncomeStatements?.[0]
+    };
 
   } catch (err) {
     console.log(err);
