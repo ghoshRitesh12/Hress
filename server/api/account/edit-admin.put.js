f6dcdb335789621a8095b205f92e051a4e535d03
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-11-17 01:22:42 +0530

    feat: add `Income` profile email updation

--- server/api/account/edit-admin.put.js
+++ server/api/account/edit-admin.put.js
@@ -1,20 +1,37 @@
+import { startSession } from "mongoose";
 import User from "~/server/models/User.model"
+import Income from "~/server/models/Income.model";
 import { serverAdminInfoSchema } from "~/utils/adminInfoSchema";
 import verifyAdminOneTimeFields from "~/server/helpers/verifyAdminOneTimeFields";
 import { nativeAuthenticate, nativeAuthorize } from "~/server/helpers/middleware/native.auth";
 
+
 export default eventHandler(async (event) => {
+  const adminSelfEditSession = await startSession();
+  adminSelfEditSession.startTransaction({
+    readConcern: "snapshot",
+    writeConcern: {
+      w: "majority",
+      journal: true
+    }
+  })
+
   try {
     await nativeAuthenticate(event);
     nativeAuthorize(event);
 
-    const queryFields = ['info', 'role'];
+    const queryFields = {
+      info: 1,
+      role: 1,
+    }
 
     const body = await readBody(event);
     await serverAdminInfoSchema.validate(body);
 
-    const foundUser = await User.findOne({ 'info.email': event?.user?.email }, queryFields)
-      .readConcern('majority')
+    const foundUser = await User.findOne({ 'info.email': event?.user?.email })
+      .session(adminSelfEditSession)
+      .select(queryFields);
+
     if (!foundUser) {
       return sendError(event, createError({
         statusCode: 404,
@@ -29,7 +46,7 @@ export default eventHandler(async (event) => {
       }))
     }
 
-    if (body.pancardNo || body.bankAccountNo || body.ifsc) {
+    if (body?.pancardNo || body?.bankAccountNo || body?.ifsc) {
       await verifyAdminOneTimeFields(
         {
           pancardNo: body.pancardNo || null,
@@ -40,30 +57,64 @@ export default eventHandler(async (event) => {
       ).catch((err) => {
         throw sendError(event, createError({
           statusCode: 400,
-          statusMessage: err.message || 'Something went wrong'
+          statusMessage: err?.message || 'Something went wrong'
         }))
       })
     }
 
-    for (const [key, value] of Object.entries(foundUser.info)) {
-      if (body[key] === value || body[key] === '') continue;
-
-      if (key in foundUser.info) {
-        foundUser.info[key] = body[key];
-      }
+    // duplicate email check
+    if (
+      body?.email !== foundUser.info.email
+      &&
+      await User.exists({ 'info.email': body?.email }).session(adminSelfEditSession)
+    ) {
+      return sendError(event, createError({
+        statusCode: 409,
+        statusMessage: "The email you are trying to set is invalid, try again"
+      }))
     }
 
-    await foundUser.save();
+    // if same email dont change income profile's email
+    if (body?.email !== foundUser.info.email) {
+      const foundIncomeProfile = await Income.findOne({ email: foundUser?.info?.email })
+        .session(adminSelfEditSession);
+      if (!foundIncomeProfile) {
+        return sendError(event, createError({
+          statusCode: 404,
+          statusMessage: 'User income profile not found'
+        }))
+      }
+
+      foundIncomeProfile.email = body.email;
+      await foundIncomeProfile.save({ session: adminSelfEditSession });
+    }
+
+
+    for (const [key, value] of Object.entries(foundUser.info)) {
+      if (body[key] === value) continue;
+
+      if (key in foundUser.info) {
+        foundUser.info[key] = body[key] === "" ? null : body[key];
+      }
+    }
+    await foundUser.save({ session: adminSelfEditSession });
+
+    await adminSelfEditSession.commitTransaction();
 
     return {
       message: 'Profile updated successfully'
     };
 
   } catch (err) {
+    await adminSelfEditSession.abortTransaction();
+
     console.log(err);
     return sendError(event, createError({
       statusCode: err?.statusCode || 500,
       statusMessage: err?.statusMessage || 'Something Went Wrong',
     }))
+    //
+  } finally {
+    await adminSelfEditSession.endSession();
   }
 })
