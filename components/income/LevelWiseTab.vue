11b7c2fcf7564501f76dbedd561da3160460eedb
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-11-17 01:50:02 +0530

    fix: update level wise tab component

--- components/income/LevelWiseTab.vue
+++ components/income/LevelWiseTab.vue
@@ -12,9 +12,9 @@
     <div
       :class="
         fc(`
-        flex flex-col gap-5 px-2 md:px-4 mx-2 pb-3 pt-4 border-[1px] 
-        border-t-0 border-zinc-700/60 rounded-bl-2xl rounded-br-2xl
-      `)
+          flex flex-col gap-5 px-2 md:px-4 mx-2 pb-3 pt-4 border-[1px] 
+          border-t-0 border-zinc-700/60 rounded-bl-2xl rounded-br-2xl
+        `)
       "
     >
       <IncomeLevel
@@ -22,21 +22,6 @@
         :level-income="props?.level?.levelIncome"
         :level-members="props?.level?.levelMembers"
       />
-
-      <!-- <div
-        class="flex items-center justify-between px-2 font-semibold text-[.95rem] gap-4"
-      >
-        <div class="leading-[1.3]">Total Level Wise Income:</div>
-
-        <div>
-          {{
-            new Intl.NumberFormat("en-IN", {
-              style: "currency",
-              currency: "INR",
-            }).format(props.totalLevelIncome || 0)
-          }}
-        </div>
-      </div> -->
     </div>
   </div>
 </template>
