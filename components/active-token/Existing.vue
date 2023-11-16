c26d85cabc7a64018903a681b821089a77afa051
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-11-17 01:17:49 +0530

    style: prettified

--- components/active-token/Existing.vue
+++ components/active-token/Existing.vue
@@ -1,19 +1,9 @@
 <template>
-  
-  <div 
-    class="text-2xl border-b-[1px] border-zinc-700/80 pb-3"
-  >
+  <div class="text-2xl border-b-[1px] border-zinc-700/80 pb-3">
     Existing Active Tokens
   </div>
 
-  <ActiveTokenCopyButton 
-    class="mt-4 w-fit ml-auto"
-  />
+  <ActiveTokenCopyButton class="mt-4 w-fit ml-auto" />
 
-  <ActiveTokenDeck 
-    class="mt-2 first-letter:mb-6 py-4"
-  />
-  
+  <ActiveTokenDeck class="mt-2 first-letter:mb-6 py-4" />
 </template>
-
-<script setup></script>
