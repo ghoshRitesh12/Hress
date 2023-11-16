ea6c9cfcb5b4bb8e8d6fc723747662304ce8b4b5
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-11-17 01:58:56 +0530

    feat: add admin view member profile team

--- pages/admin/profile/[referralId]/team.vue
+++ pages/admin/profile/[referralId]/team.vue
@@ -1,9 +1,8 @@
 <template>
-
-  <div data-account-team 
+  <div
+    data-account-team
     class="pb-12"
   >
-
     <TeamParentMember
       class="mb-6"
       v-if="data.sponsorer"
@@ -11,32 +10,37 @@
       :sponsorer-referral-id="data.sponsorer.referralId"
     />
 
-    <div 
-      class="
-      text-2xl border-b-[1px] border-zinc-700/80
-      pb-3 mt-0
-      "
-    >
-      Team
+    <div class="text-2xl border-b-[1px] border-zinc-700/80 pb-3 mt-0">Team</div>
+
+    <div class="text-zinc-400 text-[.85rem] mt-4">
+      Select a level to view the members present in that level.
     </div>
 
+    <Select
+      v-if="data?.selectLevels?.length"
+      class="w-[15rem] ml-auto mt-4"
+      placeholder="Select team level"
+      :options="data.selectLevels"
+      @input-change="fetchLevel"
+    />
 
-    <div 
-      v-if="data?.levels?.length > 0"
+    <div
+      v-if="data?.selectLevels?.length > 0"
       class="flex flex-col gap-6 mt-8"
     >
       <TeamLevel
-        v-for="level, index in data.levels"
-        :index="index"
-        :key="level.levelNo"
-        :level-no="level.levelNo"
-        :referrals="level.referrals"
-        :level-open="activeLevel === index"
-        @toggle-level="toggleLevel"
+        v-if="selectedLevelInfo"
+        :key="selectedLevelInfo.levelNo"
+        :level-no="selectedLevelInfo.levelNo"
+        :referrals="selectedLevelInfo.referrals"
+        :has-more-referrals="selectedLevelInfo.hasMore"
+        :total-no-of-referrals="selectedLevelInfo.totalReferrals"
+        :fetch-url="`/api/admin/profile/${params?.referralId}/team/${selectedLevelInfo.levelNo}`"
       />
     </div>
 
-    <Inconvenience v-else
+    <Inconvenience
+      v-else
       img-src="/images/join_team.svg"
       :img-width="512"
       :img-height="532"
@@ -47,44 +51,46 @@
     >
       Oops, this member hasn't sponsored any member yet.
     </Inconvenience>
-
   </div>
-
 </template>
 
-
 <script setup>
-
 const { params } = useRoute();
-
 useHead({
   title: `${params.referralId} - Team`,
-})
-
+});
 definePageMeta({
-  layout: 'view-profile',
-  middleware: ['native', 'admin']
-})
+  layout: "view-profile",
+  middleware: ["native", "admin"],
+});
 
 const { setPopupMessage } = usePopup();
 
-const activeLevel = ref(-1);
-const toggleLevel = (levelIndex) => {
-  activeLevel.value = levelIndex
-}
-
-const headers = useRequestHeaders(['cookie'])
 const { data, error } = await useFetch(
   `/api/admin/profile/${params.referralId}/team`,
-  { headers }
+  {
+    headers: useRequestHeaders(["cookie"]),
+  }
 );
+if (error.value) setPopupMessage(error?.value?.statusMessage);
 
-setPopupMessage(error?.value?.statusMessage)
-if(error.value) throw error;
+const selectedLevelInfo = useState(() => null);
+async function fetchLevel(selectedLevel) {
+  const { data, error, pending } = await useFetch(
+    `/api/admin/profile/${params.referralId}/team/${selectedLevel}`,
+    {
+      headers: useRequestHeaders(["cookie"]),
+    }
+  );
 
+  if (error?.value) {
+    setPopupMessage(error?.value?.statusMessage);
+    return;
+  }
+  selectedLevelInfo.value = data?.value ?? null;
+}
+
+onUnmounted(() => {
+  selectedLevelInfo.value = null;
+});
 </script>
-
-
-<style scoped>
-
-</style>
