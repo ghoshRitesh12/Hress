d76400805de69193dbac666556dd2c78994f6fba
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-11-17 01:58:06 +0530

    style: prettified code

--- pages/admin/profile/[referralId]/about.vue
+++ pages/admin/profile/[referralId]/about.vue
@@ -1,13 +1,17 @@
 <template>
-
   <div data-profile>
+    <AccountCreatedAt
+      :created-at="profile.createdAt"
+      class="mb-4"
+    />
 
     <div
-      class="
-      flex items-center gap-10
-      border-[0px] border-zinc-600
-      rounded-2xl mb-10 flex-wrap lg:flex-nowrap
-      justify-center lg:justify-normal
+      :class="
+        fc(`
+          flex items-center gap-10 border-[0px] border-zinc-600 
+          rounded-2xl mb-10 flex-wrap lg:flex-nowrap justify-center 
+          lg:justify-normal
+        `)
       "
     >
       <AccountProfileHeader
@@ -15,15 +19,8 @@
         :pfp="profile?.pfp"
       />
 
-      <div
-        class="
-        flex flex-col gap-6 text-[1.05rem]
-        w-full items-center
-        "
-      >
-        <AccountCourse
-          :course-type="profile?.courseType"
-        />
+      <div class="flex flex-col gap-6 text-[1.05rem] w-full items-center">
+        <AccountCourse :course-type="profile?.courseType" />
         <AccountVerifiedTab
           class="w-full"
           :verified="profile?.verified"
@@ -33,71 +30,45 @@
           :is-active="profile?.active"
         />
       </div>
-      
     </div>
-
-
-    <div v-if="params?.referralId?.trim()"
-      class="
-      flex items-center gap-x-10 gap-y-4 flex-wrap 
-      lg:flex-nowrap my-8
-      "
-    >
-      <AdminAccountActivateForm/>
-      <AdminAccountCourseForm/>
-    </div>
-
 
     <div
-      class="
-      flex gap-x-10 gap-y-4 flex-wrap 
-      lg:flex-nowrap my-8
-      "
+      v-if="params?.referralId?.trim()"
+      class="flex items-center gap-x-10 gap-y-4 flex-wrap lg:flex-nowrap my-8"
     >
-      <AccountRank 
-        :rank="profile?.rank"
-      />
-      <AccountReferralId
-        :referral-id="profile?.referralId"
-      />
+      <AdminAccountActivateForm />
+      <AdminAccountCourseForm />
     </div>
 
-    <AccountPersonalInfoSection
-      :info="profile?.info"
-    />
+    <div class="flex gap-x-10 gap-y-4 flex-wrap lg:flex-nowrap my-8">
+      <AccountRank :rank="profile?.rank" />
+      <AccountReferralId :referral-id="profile?.referralId" />
+    </div>
 
-    <AccountInfoEditModal/>
+    <AccountPersonalInfoSection :info="profile?.info" />
 
-    <Popup/>
+    <AccountInfoEditModal />
 
+    <Popup />
   </div>
-
 </template>
 
-
 <script setup>
-
+import fc from "~/utils/classes";
 const { params } = useRoute();
 
 useHead({
   title: `${params.referralId} - About`,
-})
+});
 definePageMeta({
-  layout: 'view-profile',
-  middleware: ['native', 'admin']
-})
-
+  layout: "view-profile",
+  middleware: ["native", "admin"],
+});
 
 const { profile, fetchProfile } = useProfile();
-await fetchProfile(`/api/admin/profile/${params.referralId}/about`)
+await fetchProfile(`/api/admin/profile/${params.referralId}/about`);
 
 onBeforeUnmount(() => {
-  profile.value = null
-})
-
+  profile.value = null;
+});
 </script>
-
-
-<style scoped>
-
-</style>
