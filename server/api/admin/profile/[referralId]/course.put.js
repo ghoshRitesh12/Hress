4fe9376101853658bdc5aa766e5c2dcaec1868f6
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-11-17 01:44:09 +0530

    refactor: removed log statement

--- server/api/admin/profile/[referralId]/course.put.js
+++ server/api/admin/profile/[referralId]/course.put.js
@@ -19,20 +19,19 @@ export default eventHandler(async (event) => {
     })
 
     const body = await readBody(event);
-    console.log(body);
     await serverCourseSchema.validate(body);
 
     const queryFields = ['courseType'];
 
     const foundUser = await User.findOne({ referralId: paramReferralId }, queryFields)
-    if(!foundUser) {
+    if (!foundUser) {
       return sendError(event, createError({
         statusCode: 404,
         statusMessage: 'User not found'
       }))
     }
 
-    if(foundUser.courseType === body.course) {
+    if (foundUser.courseType === body.course) {
       setResponseStatus(event, 200)
       return {
         message: `Course already set`
@@ -41,12 +40,12 @@ export default eventHandler(async (event) => {
 
     foundUser.courseType = body.course?.toLowerCase();
     await foundUser.save();
-    
+
     setResponseStatus(event, 200)
     return {
       message: `Course set successfully`
     };
-    
+
   } catch (err) {
     console.log(err);
     return sendError(event, createError({
