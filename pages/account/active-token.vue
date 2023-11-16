edfd565bdc4a2234aeaeaedc841e8d3c9c7610c6
Ritesh Ghosh <ghoshritesh55@gmail.com>
2023-11-17 01:54:00 +0530

    style: prettified code

--- pages/account/active-token.vue
+++ pages/account/active-token.vue
@@ -1,25 +1,19 @@
 <template>
-  
   <div data-account-active-token>
-
     <ActiveTokenExisting />
 
     <ActiveTokenGenerate />
 
     <Popup />
-
   </div>
-  
 </template>
-
 
 <script setup>
 useHead({
-  title: 'Account - Active Token'
-})
+  title: "Account - Active Token",
+});
 definePageMeta({
-  layout: 'account',
-  middleware: ['native', 'admin']
-})
-
+  layout: "account",
+  middleware: ["native", "admin"],
+});
 </script>
